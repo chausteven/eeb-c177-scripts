19706457fe08a44fcb44e87004266b92947ca42e
chausteven <schau1952@gmail.com>
2020-02-04 00:36:22 -0800

    Lecture Homework #5 Part 1

--- ExtractedBodyM.sh
+++ ExtractedBodyM.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > BodyM.csv
+tail -n +2 $1 | cut -d "," -f 2-6 | tr -s "," " " | sort -r -n -k 6 > $2
 # tail will remove the header and only take the lines starting from the second line
 # cut will seperate the data according to the delimiter ";" and only take columns 2-6
 # tr will replace all semicolin with spaces
